a50076d9be04e277cc65a007590dcd8a6dde6847
rarescadar007 <rarescadar007@gmail.com>
2024-04-17 12:03:30 +0300

    configured css

---
+++ onePageTrial/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+     <head>
+            <title>Award winning UX Design Agency | Excelence in product design</title>
+        </head>
+        <body>
+            <header>
+                    header
+            </header>
+
+            <section> 
+                s1
+            </section>
+
+            <section>
+                s2
+            </section>
+
+            <section>
+                s3
+            </section>
+
+            <section>
+              s4
+            </section>
+            
+            <section>
+                    s5
+            </section>
+            
+            <footer>
+            </footer>
+        </body>
+</template>
